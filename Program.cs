0412035b00b6e1a9ce1905f56cffd00d00f29842
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-04 15:42:32 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace findmax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5, -1, 33, 55, -5 };
+            Console.WriteLine(FindMax(a));
+        }
+        public static int FindMax(int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return -1;
+            }
+
+            int max = numbers[0];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ findmax.js
@@ -0,0 +1,18 @@
+function FindMax(numbers) {
+  if (numbers.length == 0) {
+    return -1;
+  }
+
+  var max = numbers[0];
+
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+
+  return max;
+}
+
+var a = [1, 2, 3, 4, 5, -1, 33, -5];
+console.log(FindMax(a));
